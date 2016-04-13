69b5caa0e0c7e556b27b43b10fa6fab125c1a070
philippe.dubois@alfresco.com <philippe.dubois@alfresco.com>
2016-04-13 10:58:56 +0200

    Initial of ParralelCrawler.java

--- alfresco-crawler/alfresco-crawler-repo-amp/src/main/java/org/alfresco/crawler/demoamp/ParralelCrawler.java
+++ alfresco-crawler/alfresco-crawler-repo-amp/src/main/java/org/alfresco/crawler/demoamp/ParralelCrawler.java
@@ -5,12 +5,19 @@ import org.springframework.context.ApplicationEventPublisherAware;
 
 import java.util.Date;
 
+import org.alfresco.repo.lock.JobLockService;
+import org.alfresco.repo.lock.JobLockService.JobLockRefreshCallback;
+import org.alfresco.repo.lock.LockAcquisitionException;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.security.authentication.AuthenticationUtil.RunAsWork;
 import org.alfresco.service.cmr.search.SearchService;
+import org.alfresco.service.namespace.QName;
 import org.alfresco.service.transaction.TransactionService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.alfresco.util.PropertyCheck;
 import org.alfresco.util.VmShutdownListener.VmShutdownException;
+
 /**
  * Call an action on every node found by a configured query
  * 
@@ -18,6 +25,8 @@ import org.alfresco.util.VmShutdownListener.VmShutdownException;
  */
 public class ParralelCrawler implements ApplicationEventPublisherAware
 {
+    private long LOCK_TIME_TO_LIVE = 10000;
+    private long LOCK_REFRESH_TIME = 5000;
     private static Log logger = LogFactory.getLog(ParralelCrawler.class);
     private ApplicationEventPublisher applicationEventPublisher;
     private SearchService searchService;
@@ -25,7 +34,13 @@ public class ParralelCrawler implements ApplicationEventPublisherAware
     private Date startDate;
     private Date endDate;
     private Boolean isRunning;
-    
+    private JobLockService jobLockService;
+
+    public void setJobLockService(JobLockService jobLockService)
+    {
+        this.jobLockService = jobLockService;
+    }
+
     /**
      * Perform basic checks to ensure that the necessary dependencies were injected.
      */
@@ -35,7 +50,7 @@ public class ParralelCrawler implements ApplicationEventPublisherAware
         PropertyCheck.mandatory(this, "transactionService", transactionService);
         PropertyCheck.mandatory(this, "searchService", searchService);
     }
-    
+
     /**
      * @param queryService used to retrieve older versions
      */
@@ -72,45 +87,84 @@ public class ParralelCrawler implements ApplicationEventPublisherAware
         {
             logger.debug("Starting version store cleanup.");
         }
-        refreshLock();
-        // Repeat attempts six times waiting 10 minutes between
-        startDate = new Date();
-        isRunning = true;
+
         try
         {
+            QName lockQName = QName.createQName("pc", "crawl");
+            String lockToken = jobLockService.getLock(lockQName, LOCK_TIME_TO_LIVE, 0, 1);
+            TrackerJobLockRefreshCallback callback = new TrackerJobLockRefreshCallback();
 
-            executeInternal();
-        }
-        catch (VmShutdownException e)
-        {
-            // Aborted
-            if (logger.isDebugEnabled())
+            jobLockService.refreshLock(lockToken, lockQName, LOCK_REFRESH_TIME, callback);
+            try
             {
-                logger.debug("   Content store cleanup aborted.");
+                if (logger.isTraceEnabled())
+                {
+                    logger.trace("PUSH: job lock held");
+                }
+                
+                try
+                {
+                    AuthenticationUtil.runAsSystem(new RunAsWork<Void>()
+                    {
+                        public Void doWork() throws Exception
+                        {
+                            executeInternal();
+                            return null;
+                        }
+                    });
+                }
+                catch (Exception e)
+                {
+                        // This is where push comms failure is logged on the first time
+                        logger.error("PUSH: unable to push:" + e.getMessage());
+                }
+            }
+            finally
+            {
+                if (logger.isTraceEnabled())
+                {
+                    logger.trace("PUSH: job finished");
+                }
+                
+                // Release the locks on the job and stop refreshing
+                callback.isActive = false;
+                jobLockService.releaseLock(lockToken, lockQName);
             }
         }
-        finally
+        catch (LockAcquisitionException e)
         {
-            endDate = new Date();
-            isRunning = false;
-            releaseLock();
+            if (logger.isDebugEnabled())
+            {
+                // probably already running - or repo could be read only
+                logger.debug("ParralelCrawler: unable to obtain job lock - probably already running");
+            }
         }
 
     }
-    
+
     protected void executeInternal()
     {
-        
+
     }
-    
-    protected void refreshLock()
+
+    private class TrackerJobLockRefreshCallback implements JobLockRefreshCallback
     {
-        
-    }
-    
-    protected void releaseLock()
-    {
-        
-    }
-    
+        public boolean isActive = true;
+
+        @Override
+        public boolean isActive()
+        {
+            return isActive;
+        }
+
+        @Override
+        public void lockReleased()
+        {
+            if (logger.isTraceEnabled())
+            {
+                logger.trace("lock released");
+            }
+        }
+    };
+
 }
