904e9ae7dc9a8fe1e3d2891cf20ab236e1848124
philippe.dubois@alfresco.com <philippe.dubois@alfresco.com>
2016-04-13 11:58:15 +0200

    added code to ParralelCrawler

--- alfresco-crawler/alfresco-crawler-repo-amp/src/main/java/org/alfresco/crawler/demoamp/ParralelCrawler.java
+++ alfresco-crawler/alfresco-crawler-repo-amp/src/main/java/org/alfresco/crawler/demoamp/ParralelCrawler.java
@@ -3,19 +3,29 @@ package org.alfresco.crawler.demoamp;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 
+import org.alfresco.repo.batch.BatchProcessor;
+import org.alfresco.repo.batch.BatchProcessor.BatchProcessWorker;
 import org.alfresco.repo.lock.JobLockService;
 import org.alfresco.repo.lock.JobLockService.JobLockRefreshCallback;
 import org.alfresco.repo.lock.LockAcquisitionException;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.repo.security.authentication.AuthenticationUtil.RunAsWork;
+import org.alfresco.repo.transaction.RetryingTransactionHelper.RetryingTransactionCallback;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.StoreRef;
+import org.alfresco.service.cmr.search.ResultSet;
+import org.alfresco.service.cmr.search.SearchParameters;
 import org.alfresco.service.cmr.search.SearchService;
 import org.alfresco.service.namespace.QName;
 import org.alfresco.service.transaction.TransactionService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.alfresco.util.PropertyCheck;
+import org.alfresco.util.VmShutdownListener;
 import org.alfresco.util.VmShutdownListener.VmShutdownException;
 
 /**
@@ -25,8 +35,10 @@ import org.alfresco.util.VmShutdownListener.VmShutdownException;
  */
 public class ParralelCrawler implements ApplicationEventPublisherAware
 {
+    private static VmShutdownListener vmShutdownListener = new VmShutdownListener("ParralelCrawler");
     private long LOCK_TIME_TO_LIVE = 10000;
     private long LOCK_REFRESH_TIME = 5000;
+    private int bigPageLen = 50000;
     private static Log logger = LogFactory.getLog(ParralelCrawler.class);
     private ApplicationEventPublisher applicationEventPublisher;
     private SearchService searchService;
@@ -35,6 +47,20 @@ public class ParralelCrawler implements ApplicationEventPublisherAware
     private Date endDate;
     private Boolean isRunning;
     private JobLockService jobLockService;
+    private String query;
+    private int threadNumber = 2;
+    
+    
+
+    public void setThreadNumber(int threadNumber)
+    {
+        this.threadNumber = threadNumber;
+    }
+
+    public void setQuery(String query)
+    {
+        this.query = query;
+    }
 
     public void setJobLockService(JobLockService jobLockService)
     {
@@ -99,7 +125,7 @@ public class ParralelCrawler implements ApplicationEventPublisherAware
             {
                 if (logger.isTraceEnabled())
                 {
-                    logger.trace("PUSH: job lock held");
+                    logger.trace("ParralelCrawler: job lock held");
                 }
                 
                 try
@@ -108,6 +134,8 @@ public class ParralelCrawler implements ApplicationEventPublisherAware
                     {
                         public Void doWork() throws Exception
                         {
+                            startDate = new Date();
+                            isRunning = true;
                             executeInternal();
                             return null;
                         }
@@ -116,14 +144,19 @@ public class ParralelCrawler implements ApplicationEventPublisherAware
                 catch (Exception e)
                 {
                         // This is where push comms failure is logged on the first time
-                        logger.error("PUSH: unable to push:" + e.getMessage());
+                        logger.error("ParralelCrawler: unable to push:" + e.getMessage());
+                }
+                finally
+                {
+                    endDate = new Date();
+                    isRunning = false;
                 }
             }
             finally
             {
                 if (logger.isTraceEnabled())
                 {
-                    logger.trace("PUSH: job finished");
+                    logger.trace("ParralelCrawler: job finished");
                 }
                 
                 // Release the locks on the job and stop refreshing
@@ -144,9 +177,127 @@ public class ParralelCrawler implements ApplicationEventPublisherAware
 
     protected void executeInternal()
     {
+        int startingElement = 0;
+        int lot = 0;
+        while (true)
+        {
+            lot++;
+            // search by page
+            final int staticStartingElement = startingElement;
+            // execute in READ-WRITE txn
+            RetryingTransactionCallback<Collection<NodeRef>> executeCallback = new RetryingTransactionCallback<Collection<NodeRef>>()
+                {
+                    public Collection<NodeRef> execute() throws Exception
+                    {
+                        StoreRef storeRef = new StoreRef(StoreRef.PROTOCOL_WORKSPACE, "SpacesStore");
+                        // Get VersionableNodes
+                        Collection<NodeRef> bigPageNodes = executeQuery(storeRef, searchService,
+                                query, staticStartingElement, bigPageLen);
+                        return bigPageNodes;
+                    };
+                };
 
+            try
+            {
+                if (vmShutdownListener.isVmShuttingDown())
+                {
+                    throw new VmShutdownException();
+                }
+                Collection<NodeRef> nodesToCleaned = transactionService.getRetryingTransactionHelper().doInTransaction(
+                        executeCallback, true);
+                final BatchProcessor<NodeRef> groupProcessor = new BatchProcessor<NodeRef>("VersionCleaner", this.transactionService.getRetryingTransactionHelper(), nodesToCleaned, threadNumber,
+                        5000,this.applicationEventPublisher, logger, 500);
+                
+                        
+                class NodeVersionCleaner implements BatchProcessWorker<NodeRef>
+                {
+                    public String getIdentifier(NodeRef entry)
+                    {
+                        return entry.toString();
+                    }
+
+                    public void process(NodeRef currentNode) throws Throwable
+                    {
+
+                            
+                        
+                    }
+
+                    @Override
+                    public void beforeProcess() throws Throwable
+                    {
+                        // TODO Auto-generated method stub
+                        
+                    }
+
+                    @Override
+                    public void afterProcess() throws Throwable
+                    {
+                        // TODO Auto-generated method stub
+                        
+                    }
+                }
+
+                NodeVersionCleaner unitOfWork = new NodeVersionCleaner();
+
+                groupProcessor.process(unitOfWork, true);
+
+                startingElement += bigPageLen;
+                // Done
+                if (logger.isDebugEnabled())
+                {
+                    logger.debug("  Cleaning iteration:" + staticStartingElement);
+                }
+                if (nodesToCleaned.size() < bigPageLen)
+                    break;
+            }
+            catch (VmShutdownException e)
+            {
+                // Aborted
+                if (logger.isDebugEnabled())
+                {
+                    logger.debug("Version cleanup aborted.");
+                }
+                throw e;
+            }
+            catch (Throwable e)
+            {
+                e.printStackTrace();
+                logger.warn("System shutting down during version cleaning at:" + staticStartingElement);
+                break;
+            }
+        }
     }
 
+    private Collection<NodeRef> executeQuery(
+            StoreRef storeRef,
+            SearchService searchService,
+            String query,
+            int startingElement,
+            int pageLen)
+    {
+
+        SearchParameters sp = new SearchParameters();
+        sp.addStore(storeRef);
+        sp.setLanguage(SearchService.LANGUAGE_FTS_ALFRESCO);
+        sp.setSkipCount(startingElement);
+        // -1 unlimited result size
+        sp.setMaxItems(-1);
+        sp.setQuery(query);
+        ResultSet results = searchService.query(sp);
+        Collection<NodeRef> nodeToClean = new ArrayList<NodeRef>(pageLen);
+        int i;
+        for (i = startingElement; i < startingElement + pageLen; i++)
+        {
+            if (i - startingElement >= results.length())
+                break;
+            NodeRef nodeRef = results.getNodeRef(i - startingElement);
+            nodeToClean.add(nodeRef);
+        }
+        results.close();
+        return nodeToClean;
+    }
+    
     private class TrackerJobLockRefreshCallback implements JobLockRefreshCallback
     {
         public boolean isActive = true;
